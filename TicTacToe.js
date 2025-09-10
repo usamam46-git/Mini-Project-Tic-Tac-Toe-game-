7f2ba5627c7b9cbc44848c599be97857aa853477
usamam46-git <usamam46@gmail.com>
2025-09-11 01:50:12 +0500

    Done

---
+++ TicTacToe.js
@@ -0,0 +1,93 @@
+let boxes = document.querySelectorAll(".btn");
+let resetButton = document.getElementsByClassName("reset");
+let winner = document.getElementById("winner");
+let reset = document.querySelector(".reset");
+
+let turn0 = true;
+
+const winningPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked!");
+        if (turn0) {
+            box.innerText = "O";
+            turn0 = false
+        } else {
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    }
+    )
+}
+)
+
+function disabled() {
+    for (const box of boxes) {
+        box.disabled = true;
+
+    }
+}
+function enabled() {
+    for (const box of boxes) {
+        box.disabled = false;
+
+    }
+}
+
+reset.addEventListener("click", () => {
+    winner.innerText = "";
+    boxes.forEach((box) => {
+        box.innerText = "";
+        enabled();
+    }
+    )
+    newGame.remove();
+}
+)
+
+let newGame = document.createElement("button");
+newGame.style.backgroundColor = "#FD970D"
+newGame.style.color = "white";
+newGame.style.padding = "8px";
+newGame.innerText = "New Game"
+newGame.style.borderRadius = "15px";
+newGame.style.border = "none";
+newGame.addEventListener("click", () => {
+    winner.innerText = "";
+    boxes.forEach((box) => {
+        box.innerText = "";
+        enabled();
+    }
+    )
+    newGame.remove();
+}
+)
+
+function checkWinner() {
+    for (const pattern of winningPattern) {
+        let postVal1 = boxes[pattern[0]].innerText;
+        let postVal2 = boxes[pattern[1]].innerText;
+        let postVal3 = boxes[pattern[2]].innerText;
+
+        if (postVal1 !== "" && postVal2 !== "" && postVal3 !== "") {
+            if (postVal1 === postVal2 && postVal2 === postVal3) {
+                winner.innerText = `Winner🎉 ${postVal1}`;
+                disabled();
+                document.querySelector(".new-game").appendChild(newGame);
+            }
+        }
+
+    }
+}
